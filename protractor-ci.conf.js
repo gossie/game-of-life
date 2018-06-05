2b13a82382f6ec7f7ec3680ebd43c11361f83bd3
André Schreck <andre.schreck@gmail.com>
2018-06-05 22:56:04 +0200

    headless

--- protractor-ci.conf.js
+++ protractor-ci.conf.js
@@ -9,7 +9,10 @@ exports.config = {
         './e2e/**/*.e2e-spec.ts'
     ],
     capabilities: {
-        'browserName': 'ChromeHeadless'
+        'browserName': 'chrome',
+        chromeOptions: {
+            args: [ "--headless" ]
+        }
     },
     directConnect: true,
     baseUrl: 'http://localhost:4200/',
