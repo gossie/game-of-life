b2ef427c219285f5bccc76fc8ce6ac68d219bb05
André Schreck <andre.schreck@gmail.com>
2018-06-30 17:34:12 +0200

    e2e tests

--- e2e/game-of-life.e2e-spec.ts
+++ e2e/game-of-life.e2e-spec.ts
@@ -3,25 +3,28 @@ import { AppPage } from './page/app.po';
 import { FieldComponent } from './page/field.co';
 import { OptionsComponent } from './page/options.co';
 
+const expectThatAllCellsAreDead = async (field: FieldComponent, width: number, height: number) => {
+    expect(await field.getNumberOfRows()).toBe(height);
+    for (let i = 0; i < height; i++) {
+        expect(await field.getRow(i).getNumberOfCells()).toBe(width);
+        for (let j = 0; j < width; j++) {
+            expect(await field.getRow(i).getCell(j).isDead()).toBeTruthy();
+        }
+    }
+};
+
 describe('game-of-life Field', () => {
     let page: AppPage;
 
     beforeEach(() => {
-        browser.waitForAngularEnabled(false);
         page = new AppPage();
         page.navigateTo();
     });
 
     it('should have 17 rows and each row should have 25 columns', async () => {
         const field: FieldComponent = page.getField();
-        expect(await field.getNumberOfRows()).toBe(17);
 
-        for (let i = 0; i < 17; i++) {
-            expect(await field.getRow(i).getNumberOfCells()).toBe(25);
-            for (let j = 0; j < 25; j++) {
-                expect(await field.getRow(i).getCell(j).isDead()).toBeTruthy();
-            }
-        }
+        expectThatAllCellsAreDead(field, 25, 17);
     });
 
     it('should select a start configuration', async () => {
@@ -42,9 +45,55 @@ describe('game-of-life Field', () => {
         expect(await page.getOptions().isWidthEnabled()).toBeTruthy();
         expect(await page.getOptions().isHeightEnabled()).toBeTruthy();
 
+        browser.waitForAngularEnabled(false);
+
         await page.startGame();
 
         expect(await page.getOptions().isWidthEnabled()).toBeFalsy();
         expect(await page.getOptions().isHeightEnabled()).toBeFalsy();
+
+        browser.waitForAngularEnabled(true);
+    });
+
+    it('should reset the field when width is set', async () => {
+        const field: FieldComponent = page.getField();
+
+        await field.getRow(4).getCell(4).select();
+        await field.getRow(5).getCell(5).select();
+        await field.getRow(6).getCell(5).select();
+        await field.getRow(6).getCell(4).select();
+        await field.getRow(6).getCell(3).select();
+
+        expect(await field.getRow(4).getCell(4).isAlive()).toBeTruthy();
+        expect(await field.getRow(5).getCell(5).isAlive()).toBeTruthy();
+        expect(await field.getRow(6).getCell(5).isAlive()).toBeTruthy();
+        expect(await field.getRow(6).getCell(4).isAlive()).toBeTruthy();
+        expect(await field.getRow(6).getCell(3).isAlive()).toBeTruthy();
+
+        await page.getOptions().clearWidth();
+        await page.getOptions().setWidth(20);
+
+        expectThatAllCellsAreDead(field, 20, 17);
+    });
+
+    it('should reset the field when height is set', async () => {
+        const field: FieldComponent = page.getField();
+
+        await field.getRow(4).getCell(4).select();
+        await field.getRow(5).getCell(5).select();
+        await field.getRow(6).getCell(5).select();
+        await field.getRow(6).getCell(4).select();
+        await field.getRow(6).getCell(3).select();
+
+        expect(await field.getRow(4).getCell(4).isAlive()).toBeTruthy();
+        expect(await field.getRow(5).getCell(5).isAlive()).toBeTruthy();
+        expect(await field.getRow(6).getCell(5).isAlive()).toBeTruthy();
+        expect(await field.getRow(6).getCell(4).isAlive()).toBeTruthy();
+        expect(await field.getRow(6).getCell(3).isAlive()).toBeTruthy();
+
+        await page.getOptions().clearHeight();
+        await page.getOptions().setHeight(15);
+
+        expectThatAllCellsAreDead(field, 25, 15);
     });
 });
--- e2e/page/options.co.ts
+++ e2e/page/options.co.ts
@@ -15,4 +15,28 @@ export class OptionsComponent {
     public isHeightEnabled(): promise.Promise<boolean> {
         return this.el.element(by.css('#height')).isEnabled();
     }
+
+    public clearWidth(): promise.Promise<void> {
+        return this.el.element(by.css('#width')).clear();
+    }
+
+    public setWidth(width: number): promise.Promise<void> {
+        return this.el.element(by.css('#width')).sendKeys(width);
+    }
+
+    public clearHeight(): promise.Promise<void> {
+        return this.el.element(by.css('#height')).clear();
+    }
+
+    public setHeight(height: number): promise.Promise<void> {
+        return this.el.element(by.css('#height')).sendKeys(height);
+    }
+
+    public setTick(tick: number): void {
+        this.el.element(by.css('#tick')).sendKeys(tick);
+    }
+
+    public setRandom(random: number): void {
+        this.el.element(by.css('#random')).sendKeys(random);
+    }
 }
