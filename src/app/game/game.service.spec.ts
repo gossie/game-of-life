40e51e5fdfd14d568c384865344d316ceb2ed103
André Schreck <andre.schreck@gmail.com>
2018-07-01 17:55:36 +0200

    unit test for GameService

--- src/app/game/game.service.spec.ts
+++ src/app/game/game.service.spec.ts
@@ -1,15 +1,39 @@
 import { TestBed, inject } from '@angular/core/testing';
-
+import { GameStartedEvent } from './game-started-event';
+import { GameRunningEvent } from './game-running-event';
+import { GamePausedEvent } from './game-paused-event';
 import { GameService } from './game.service';
 
 describe('GameService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [GameService]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [GameService]
+        });
     });
-  });
 
-  it('should be created', inject([GameService], (service: GameService) => {
-    expect(service).toBeTruthy();
-  }));
+    it('should be created', inject([GameService], (service: GameService) => {
+        expect(service).toBeTruthy();
+    }));
+
+    it('should listen to events', done => {
+        inject([GameService], (service: GameService) => {
+            let index = 0;
+            service.observe().subscribe(event => {
+                if (index == 0) {
+                    index++;
+                    expect(event).toEqual(new GameStartedEvent());
+                } else if (index == 1) {
+                    index++;
+                    expect(event).toEqual(new GameRunningEvent(0));
+                    service.pauseGame();
+                } else if (index == 2) {
+                    index++;
+                    expect(event).toEqual(new GamePausedEvent());
+                    done();
+                }
+            });
+
+            service.startGame(500);
+        })();
+    });
 });
