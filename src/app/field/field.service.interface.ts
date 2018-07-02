d51f6e0b4797262975332e461762b27fc502bfd5
André Schreck <gossie@users.noreply.github.com>
2018-07-02 23:50:50 +0200

    Update field.service.interface.ts

--- src/app/field/field.service.interface.ts
+++ src/app/field/field.service.interface.ts
@@ -1,5 +1,5 @@
-import {Observable} from 'rxjs/Observable';
-import {Field} from './field';
+import { Observable } from 'rxjs/Observable';
+import { Field } from './field';
 
 export interface FieldServiceInterface {
 
