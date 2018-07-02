3fee3268db1c0f471e7b12a40012ca046e6556b9
André Schreck <andre.schreck@gmail.com>
2018-07-02 22:58:03 +0200

    game module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { GameService } from './game/game.service';
 import { OptionsModule } from './options/options.module';
 import { FieldModule } from './field/field.module';
 import { StoreHolder } from './store.holder';
+import { GameModule } from './game/game.module';
 
 
 @NgModule({
@@ -15,13 +15,15 @@ import { StoreHolder } from './store.holder';
     imports: [
         BrowserModule,
         FormsModule,
+        GameModule,
         OptionsModule,
         FieldModule
     ],
     providers: [
-        GameService,
         StoreHolder
     ],
-    bootstrap: [AppComponent]
+    bootstrap: [
+        AppComponent
+    ]
 })
 export class AppModule { }
